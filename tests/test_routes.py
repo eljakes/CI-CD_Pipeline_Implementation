00642a4ed26d19881263b41c84c0c441f7fb76b8
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2024-07-01 19:34:24 -0400

    {commit message here}

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -129,7 +129,7 @@ class TestAccountService(TestCase):
 
     def test_get_account(self):
         """It should Read a single Account"""
-        account = self._create_accounts(1)[1]
+        account = self._create_accounts(1)[0]
         resp = self.client.get(
             f"{BASE_URL}/{account.id}", content_type="application/json"
         )
