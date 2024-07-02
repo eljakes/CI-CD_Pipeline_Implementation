1a1f1eedfafd79c1e8bfb25d8b8123bc43a3dd28
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2024-07-01 22:53:21 -0400

    complete CI build

--- service/routes.py
+++ service/routes.py
@@ -7,6 +7,7 @@ from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
+
 ############################################################
 # Health Endpoint
 ############################################################
@@ -85,7 +86,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    return account.serialize(), status.HTTP_200_OK    
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
